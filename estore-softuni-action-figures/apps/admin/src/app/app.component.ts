c5d3ca22e98e11fcd35a8b4eaed400add6ddfb96
KIKonstantin <kikonstantin@outlook.com>
2022-12-04 19:05:15 +0200

    init angular project;Ipackages;structure c, styles

---
+++ estore-softuni-action-figures/apps/admin/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'estore-softuni-action-figures-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    title = 'admin';
+}
